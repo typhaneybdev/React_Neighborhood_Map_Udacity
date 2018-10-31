f61e0989a482bc474d85c7f2d73b0d587cab9150
Typhaney-Shanker <typhaney@gmail.com>
2018-10-30 22:02:36 -0700

    markers added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { 
-  load_google_maps, 
-  load_places 
-} from "./utils"
+import { load_google_maps, load_places } from "./utils";
 
 //https://www.youtube.com/watch?v=Uw5Ij56RhME eman mohammed abd elsalam Zaghlul Streamed live on Jul 28, 2018
 //https://developers.google.com/maps/documentation/javascript/tutorial
@@ -15,20 +12,34 @@ class App extends Component {
 
     Promise.all([googleMapsPromise, placesPromise]).then(values => {
       let google = values[0];
+      let venues = values[1].response.venues;
+      console.log(values);
 
       this.google = google;
+      this.markers = [];
       this.map = new google.maps.Map(document.getElementById("map"), {
         zoom: 9,
         scrollwheel: true,
         center: { lat: 34.86974, lng: -111.76099 }
       });
+
+      venues.forEach(venue => {
+        let marker = new google.maps.Marker({
+          position: {lat: venue.location.lat, lng: venue.location.lng },
+          map: this.map,
+          venue: venue,
+          id: venue.id,
+          name: venue.name,
+          animation: google.maps.Animation.DROP
+
+        });
+      }); 
+
+
     });
   }
   render() {
-    return (
-      <div id="map">
-      </div>
-    );
+    return <div id="map" />;
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,8 @@
-import axios from "axios";
+
 //https://www.youtube.com/watch?v=5J6fs_BlVC0&feature=youtu.be Ryan Waite Walk through 10/16
 //Ryan Waite wrote and recommended to use the following code to load google map,
 //link to repo https://github.com/ryanwaite28/script-store/blob/master/js/react_resolve_google_maps.js
 //https://www.youtube.com/watch?v=dAhMIF0fNpo Elharoney walk through 8/17/18 foursquare Api
-//https://github.com/axios/axios promise based http client for browser and node.js
 //https://developer.foursquare.com/ 3rd party location API
 export function load_google_maps() {
   return new Promise(function(resolve, reject) {
@@ -24,21 +23,9 @@ export function load_google_maps() {
 }
 
 export function load_places() {
-  const endPoint = "https://api.foursquare.com/v2/venues/explore?";
-  const parameters = {
-    client_id: "TZXA1I2LTTCSDSCHY0K31HPIBLDZ3GJCFZ1GIX2HWWHYL3SI",
-    client_secret: "LDTK5GXKLVQT4AGOKPIRJIRDTQGRFNYWJBKTMGW0ZYTQQ4UP",
-    query: "outdoors",
-    near: "Sedona, AZ",
-    v: "20182410"
-  };
-
-  axios
-    .get(endPoint + new URLSearchParams(parameters))
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log("ERROR!! " + error);
-    });
+  let city = "Sedona, AZ";
+  let query = "trail";
+  var apiURL =
+  'https://api.foursquare.com/v2/venues/search?client_id=TZXA1I2LTTCSDSCHY0K31HPIBLDZ3GJCFZ1GIX2HWWHYL3SI&client_secret=LDTK5GXKLVQT4AGOKPIRJIRDTQGRFNYWJBKTMGW0ZYTQQ4UP&v=20180323&limit=50&near=' + city + '&query=' + query + '';
+  return fetch(apiURL).then(resp => resp.json());
 }
